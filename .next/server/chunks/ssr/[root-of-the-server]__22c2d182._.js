a92c506f570ebc9ad6d601466f9769b3d2459dd0
jizzy-joey <ebubelewu@gmail.com>
2025-07-17 03:38:12 +0100

    Add performance tracing data for various job pages and user actions
    - Enhanced trace data for job-related pages including ensure-page and handle-request metrics.
    - Included memory usage statistics for improved performance monitoring.
    - Added new entries for the signup and login pages, capturing their respective request durations and memory usage.
    - Updated trace data for the growth marketing manager job page and other related endpoints.

--- .next/server/chunks/ssr/[root-of-the-server]__22c2d182._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__22c2d182._.js
@@ -189,7 +189,7 @@ function Button({ title }) {
     const router = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$navigation$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["useRouter"])();
     const handleClick = ()=>{
         const slug = typeof title === 'string' ? title.toLowerCase().replace(/\s+/g, '-') : '';
-        router.push(`/DashNav/${slug}`);
+        router.push(`//${slug}`);
     // This will navigate to the job description page with the title as a slug
     };
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("button", {
